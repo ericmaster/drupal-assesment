41006c317b0f1d3086e534242d2639695ac08983
Eric Aguayo <ericmaster@nimblersoft.com>
2025-06-18 23:48:09 +0000

    Added ContentEntityNormalizer class and view with REST display

---
+++ web/modules/custom/custom_module/src/Normalizer/NodeNormalizer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\custom_module\Normalizer;
+
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityTypeRepositoryInterface;
+use Drupal\node\NodeInterface;
+use Drupal\serialization\Normalizer\ContentEntityNormalizer;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+/**
+ * Custom normalizer for Node entities that adds a "velir" attribute.
+ */
+class NodeNormalizer extends ContentEntityNormalizer {
+
+  /**
+   * The interface or class that this Normalizer supports.
+   */
+  protected array $supportedInterfaceOrClass = [NodeInterface::class];
+
+  /**
+   * Constructs a NodeNormalizer object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityTypeRepositoryInterface $entityTypeRepository
+   *   The entity type repository.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entityFieldManager
+   *   The entity field manager.
+   */
+  public function __construct(
+    #[Autowire(service: 'entity_type.manager')]
+    EntityTypeManagerInterface $entityTypeManager,
+    #[Autowire(service: 'entity_type.repository')]
+    EntityTypeRepositoryInterface $entityTypeRepository,
+    #[Autowire(service: 'entity_field.manager')]
+    EntityFieldManagerInterface $entityFieldManager,
+  ) {
+    parent::__construct($entityTypeManager, $entityTypeRepository, $entityFieldManager);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($object, $format = NULL, array $context = []): array|string|int|float|bool|\ArrayObject|NULL {
+    $data = parent::normalize($object, $format, $context);
+
+    // Add the custom "velir" attribute with value "212"
+    if (is_array($data)) {
+      $data['velir'] = '212';
+    }
+
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function supportsNormalization($data, $format = NULL, array $context = []): bool {
+    return $data instanceof NodeInterface;
+  }
+
+}
